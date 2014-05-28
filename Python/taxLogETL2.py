4d87e6253d0eb5360912de8384b99b5678d61acf
JJLight <fantasycloud6@gmail.com>
2014-05-28 21:00:55 +0800

    Add User Agent parser

--- Python/taxLogETL2.py
+++ Python/taxLogETL2.py
@@ -1,1 +1,54 @@
-import datetime
+import sys
+import urlparse
+import os
+import httpagentparser
+agentParser = httpagentparser
+# You need install httpagentparser in each datanode.
+#git clone https://github.com/shon/httpagentparser.git
+#cd httpagentparser/
+#su
+#yum install  python-setuptools
+#sudo python setup.py install
+
+
+#add parseUserAgent
+for line in sys.stdin:
+    #f.write(line)
+    line = line.strip()
+    client_ip,client,userid,request,status,bytes_sent, date_time,referer,useragent,host = line.split('\t')
+    if(len(request.split(' ')) == 3):
+        method,uri,protocal = request.split(' ')
+    else:
+        method = "-"
+        uri = "-"
+        protocal = "-"
+    pr = urlparse.urlparse(uri)
+    path = pr.path
+    params = pr.params
+    query = pr.query
+    fileType = os.path.splitext(path)[-1]
+    if fileType:
+        fileName = path.split("/")[-1]
+    else:
+        fileName = "-"
+        fileType = "-"
+        
+    simple_data = agentParser.simple_detect(useragent)
+    rich_data = agentParser.detect(useragent)
+    
+    agent_os = simple_data[0]
+    agent_browser = simple_data[1]
+    browserName = str(rich_data['browser']['name']) if (('browser' in rich_data) and ('name' in rich_data['browser'])) else "-"
+    browserVersion = str(rich_data['browser']['version']) if (('browser' in rich_data) and ('version' in rich_data['browser'])) else "-"
+    
+     #like iOS , Windows ,Android
+    platformName = str(rich_data['platform']['name']) if (('platform' in rich_data) and ('name' in rich_data['platform'])) else "-"
+    platformVersion = str(rich_data['platform']['version'])   if (('platform' in rich_data) and ('version' in rich_data['platform'])) else "-"
+    #like iPhone, Ipad
+    distName = str(rich_data['dict']['name']) if (('dict' in rich_data) and ('name' in rich_data['dict'])) else "-"
+    distVersion = str(rich_data['dict']['version']) if (('dict' in rich_data) and ('version' in rich_data['dict'])) else "-"
+
+    bot = str(rich_data['bot']) if ('bot' in rich_data) else "-"
+    
+    print "\t".join([client_ip,client,userid,request,method,uri,protocal,path,params,query,fileType,fileName,status,bytes_sent, date_time,referer,useragent,agent_os,agent_browser,browserName,browserVersion,platformName,platformVersion,distName,distVersion,bot,host])
+    
---
+++ Python/taxLogHive2Impala2.py
@@ -0,0 +1,102 @@
+import datetime
+import hadoopy
+from subprocess import call
+#hive -e 'use tax;add jar /opt/cloudera/parcels/CDH/lib/hive/lib/hive-contrib-0.10.0-cdh4.4.0.jar;drop table tax_access_log;CREATE TABLE tax_access_log ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.columnar.ColumnarSerDe" STORED AS RCFile AS SELECT client_ip,client,userid,request,status,bytes_sent, from_unixtime(unix_timestamp(date_time,"dd/MMM/yyyy:hh:mm:ss")) as date_time,referer,useragent,host FROM tax_access_log_raw where date_time is not NULL;'
+#impala-shell -i 'dn1' -q 'use tax;refresh tax_access_log;select host,max(date_time),count(*) from  tax.tax_access_log group by host limit 20';
+t1 = datetime.datetime.now()-datetime.timedelta(hours=3)
+t2 = datetime.datetime.now()-datetime.timedelta(hours=2)
+t3 = datetime.datetime.now()-datetime.timedelta(hours=1)
+# example '2014-05-26 18%'
+queryDate = [t1.strftime('%Y-%m-%d')+'%'] + [t2.strftime('%Y-%m-%d')+'%'] + [t3.strftime('%Y-%m-%d')+'%']
+deleteTime = [t1.strftime('%Y-%m-%d %H')+'%3A00%3A00'] + [t2.strftime('%Y-%m-%d %H')+'%3A00%3A00'] + [t3.strftime('%Y-%m-%d %H')+'%3A00%3A00']
+insertTime = [t1.strftime('%Y-%m-%d %H')+'%'] + [t2.strftime('%Y-%m-%d %H')+'%'] + [t3.strftime('%Y-%m-%d %H')+'%']
+
+
+#hiveStatementForCreate = "use tax;add jar /opt/cloudera/parcels/CDH/lib/hive/lib/hive-contrib-0.10.0-cdh4.4.0.jar;drop table tax_access_log_textFile;CREATE TABLE tax_access_log_textFile AS SELECT client_ip,client,userid,request,status,bytes_sent, from_unixtime(unix_timestamp(date_time,'dd/MMM/yyyy:hh:mm:ss')) as date_time,referer,useragent,host FROM tax_access_log_raw where date_time is not NULL;"
+#####1.trans datetime format.
+hiveStatementForCreate = "use tax;"
+hiveStatementForCreate += "add jar /opt/cloudera/parcels/CDH/lib/hive/lib/hive-contrib-0.10.0-cdh4.4.0.jar;"
+hiveStatementForCreate += "drop table tax_access_log_textFile;"
+hiveStatementForCreate += "CREATE TABLE tax_access_log_textFile AS SELECT client_ip,client,userid,request,status,bytes_sent, from_unixtime(unix_timestamp(date_time,'dd/MMM/yyyy:HH:mm:ss')) as date_time,referer,useragent,host FROM tax_access_log_raw"
+hiveStatementForCreate += " where date_time is not NULL "
+hiveStatementForCreate += " AND("
+tempStatement =[]
+for query_date in queryDate :
+    tempStatement += [" from_unixtime(unix_timestamp(date_time,'dd/MMM/yyyy:HH:mm:ss')) like '"+query_date+"'"]
+
+hiveStatementForCreate += " or ".join(tempStatement)
+hiveStatementForCreate += ");"
+
+print "hiveStatementForCreate:"+hiveStatementForCreate;
+hivestrcommand = ["hive","-e",hiveStatementForCreate]
+current1 = datetime.datetime.now() 
+call(hivestrcommand)
+current2 = datetime.datetime.now()
+print "hive1 second="+str((current2 - current1).seconds)
+
+
+#hiveStatementForPythonCreate = "use tax;add FILE /home/hadoop/taxlogETL/taxlogETL.py;drop table tax_access_log_python;CREATE TABLE tax_access_log_python  AS select TRANSFORM (client_ip,client,userid,request,status,bytes_sent, date_time,referer,useragent,host )  USING 'python taxlogETL.py'  AS (client_ip,client,userid,request,method,uri,protocal,path,params,query,fileType,fileName,status,bytes_sent,date_time,referer,useragent,host ) FROM tax_access_log_textFile where date_time is not NULL;"
+#####2.use python ( max last 2 day)
+hiveStatementForPythonCreate = "use tax;"
+hiveStatementForPythonCreate += "add FILE /home/hadoop/taxlogETL/taxLogETL2.py;"
+hiveStatementForPythonCreate += "drop table tax_access_log_python_min;"
+hiveStatementForPythonCreate += "CREATE TABLE tax_access_log_python_min "
+hiveStatementForPythonCreate += " AS select TRANSFORM (client_ip,client,userid,request,status,bytes_sent, date_time,referer,useragent,host ) "
+hiveStatementForPythonCreate += " USING 'python taxLogETL2.py' "
+hiveStatementForPythonCreate += " AS (client_ip,client,userid,request,method,uri,protocal,path,params,query,fileType,fileName,status,bytes_sent,date_time,referer,"
+hiveStatementForPythonCreate += "useragent,agent_os,agent_browser,browserName,browserVersion,platformName,platformVersion,distName,distVersion,bot"
+hiveStatementForPythonCreate += ",host ) "
+hiveStatementForPythonCreate += " FROM tax_access_log_min where date_time is not NULL "
+hiveStatementForPythonCreate += " AND ("
+tempStatement =[]
+for query_date_time in queryDate :
+    tempStatement += ["date_time like '"+query_date_time+"'"]
+
+hiveStatementForPythonCreate += " or ".join(tempStatement)
+hiveStatementForPythonCreate += ");"
+
+print "hiveStatementForPythonCreate:"+hiveStatementForPythonCreate;
+hivestrcommandForPython = ["hive","-e",hiveStatementForPythonCreate]
+current2 = datetime.datetime.now()
+call(hivestrcommandForPython)
+current3 = datetime.datetime.now()
+print "hive2 second="+str((current3 - current2).seconds)
+
+#impalaStatementForCreate = "use tax;refresh tax.tax_access_log_python;insert overwrite TABLE tax_access_log_partition PARTITION (date_hour) SELECT client_ip,client,userid,request,method,uri,protocal,path,params,query,fileType,fileName,status,bytes_sent, date_time,referer,useragent,host,concat(strleft(from_unixtime(unix_timestamp(date_time)),14),'00:00')as date_hour from  tax.tax_access_log_python;";
+#####3.delete old data
+for deltime in deleteTime :
+    hdfsFilePath = '"/user/hive/warehouse/tax.db/tax_access_log_partition/date_hour='+deltime+'"'
+    if hadoopy.exists(hdfsFilePath) == 1:
+        print "remove file path:"+hdfsFilePath
+        hadoopy.rmr('"/user/hive/warehouse/tax.db/tax_access_log_partition/date_hour='+deltime+'"')
+
+#####4.insert Impala
+impalaStatementForCreate = "use tax;refresh tax.tax_access_log_python;"
+impalaStatementForCreate += " insert into TABLE tax_access_log_partition PARTITION (date_hour) "
+impalaStatementForCreate += " SELECT client_ip,client,userid,request,method,uri,protocal,path,params,query,fileType,fileName,status,bytes_sent, date_time,referer,useragent,host,concat(strleft(from_unixtime(unix_timestamp(date_time)),14),'00:00')as date_hour "
+impalaStatementForCreate += " from  tax.tax_access_log_python"
+impalaStatementForCreate += " where "
+
+tempStatement =[]
+for insert_time in insertTime :
+    tempStatement += ["date_time like '"+insert_time+"'"]
+
+impalaStatementForCreate += " or ".join(tempStatement)
+impalaStatementForCreate += ";"
+
+print "impalaStatementForCreate:"+impalaStatementForCreate;
+impalaStrcommand = ["impala-shell","-i","dn1","-q",impalaStatementForCreate];
+current2 = datetime.datetime.now()
+call(impalaStrcommand)
+current3 = datetime.datetime.now()
+print "impala1 second="+str((current3 - current2).seconds)
+
+#####5.check Impala
+impalaStatementForSelect = "use tax;refresh tax.tax_access_log_partition;select host,max(date_time),count(*) from  tax.tax_access_log_partition group by host limit 20;";
+impalaStrcommand = ["impala-shell","-i","dn1","-q",impalaStatementForSelect];
+current2 = datetime.datetime.now()
+call(impalaStrcommand)
+current3 = datetime.datetime.now()
+print "impala2 second ="+str((current3 - current2).seconds)
+
+print "total second = "+str((current3 - current1).seconds)
